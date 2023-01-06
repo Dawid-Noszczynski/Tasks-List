8237cc8052300706ae779c43ad74783a61388028
Dawid Noszczynski <dawid.noszczynski@icloud.com>
2023-01-06 19:53:39 +0100

    render function added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
 {
-   const onFormSubmit = (event) => {
-    event.preventDefault();
-   } 
-   
+    const tasks = [
+        {
+            content: "task 1",
+            done: true,
+        },
+        {
+            content: "task 2",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
+    };
+
+
+    //    const onFormSubmit = (event) => {
+    //     event.preventDefault();
+    //    } 
+
     const init = () => {
-        const formElement = document.querySelector(".js-form");
+        // const formElement = document.querySelector(".js-form");
 
-        formElement.addEventListener("submit", onFormSubmit);
-    }
+        // formElement.addEventListener("submit", onFormSubmit);
 
-    init()
+        render();
+    };
+
+    init();
 }
