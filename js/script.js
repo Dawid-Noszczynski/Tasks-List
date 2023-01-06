eb17c050b79fa0473336eddbae1d78f70dceb2c7
Dawid Noszczynski <dawid.noszczynski@icloud.com>
2023-01-06 19:21:11 +0100

    init function added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+{
+   const onFormSubmit = (event) => {
+    event.preventDefault();
+   } 
+   
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init()
+}
